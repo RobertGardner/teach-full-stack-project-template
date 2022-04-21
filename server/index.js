313753e183c05a0192a9caf91ba50d022e928613
Tim Davis <7432943+thebearingedge@users.noreply.github.com>
2022-04-21 23:59:22 +0000

    refactor server/index.js

--- server/index.js
+++ server/index.js
@@ -1,13 +1,13 @@
 require('dotenv/config');
-const path = require('path');
 const express = require('express');
 const errorMiddleware = require('./error-middleware');
 const staticMiddleware = require('./static-middleware');
 
 const app = express();
-const publicPath = path.join(__dirname, 'public');
 
 if (process.env.NODE_ENV === 'development') {
+  const path = require('path');
+  const publicPath = path.join(__dirname, 'public');
   app.use(require('./dev-middleware')(publicPath));
 }
 
@@ -17,5 +17,5 @@ app.use(errorMiddleware);
 
 app.listen(process.env.PORT, () => {
   // eslint-disable-next-line no-console
-  console.log(`express server listening on port ${process.env.PORT}`);
+  console.log(`\napp listening on port ${process.env.PORT}\n`);
 });
