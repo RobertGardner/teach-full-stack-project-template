68550b9bd604a47b0c353b1ad5caccd7d79b7f42
Tim Davis <7432943+thebearingedge@users.noreply.github.com>
2022-05-19 00:36:17 +0000

    TIL, we are _not_ ready for <StrictMode>

--- client/index.jsx
+++ client/index.jsx
@@ -5,8 +5,4 @@ import App from './app';
 const container = document.querySelector('#root');
 const root = ReactDOM.createRoot(container);
 
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<App />);
