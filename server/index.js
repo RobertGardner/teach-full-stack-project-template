a35154566bed3bd617b0e11aa8915554a8d77546
Tim Davis <7432943+thebearingedge@users.noreply.github.com>
2022-08-16 16:10:09 +0000

    simplify server entrypoint

--- server/index.js
+++ server/index.js
@@ -1,16 +1,11 @@
 require('dotenv/config');
-const path = require('path');
 const express = require('express');
+const staticMiddleware = require('./static-middleware');
 const errorMiddleware = require('./error-middleware');
 
 const app = express();
-const publicPath = path.join(__dirname, 'public');
 
-if (process.env.NODE_ENV === 'development') {
-  app.use(require('./dev-middleware')(publicPath));
-} else {
-  app.use(express.static(publicPath));
-}
+app.use(staticMiddleware);
 
 app.get('/api/hello', (req, res) => {
   res.json({ hello: 'world' });
--- server/static-middleware.js
+++ server/static-middleware.js
@@ -1,4 +1,17 @@
-module.exports = function devMiddleware(publicPath) {
+const path = require('path');
+const express = require('express');
+
+const publicPath = path.join(__dirname, 'public');
+
+const staticMiddleware = module.exports = [
+  express.static(publicPath)
+];
+
+if (process.env.NODE_ENV === 'development') {
+  staticMiddleware.unshift(...devMiddleware());
+}
+
+function devMiddleware() {
   const livereload = require('livereload').createServer();
   livereload.server.once('connection', () => {
     setTimeout(() => livereload.sendAllClients(JSON.stringify({
@@ -11,7 +24,6 @@ module.exports = function devMiddleware(publicPath) {
   return [
     require('connect-livereload')(),
     require('webpack-dev-middleware')(webpack),
-    require('webpack-hot-middleware')(webpack),
-    require('express').static(publicPath)
+    require('webpack-hot-middleware')(webpack)
   ];
-};
+}
