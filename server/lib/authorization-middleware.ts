21cff1ed25d3770f5a725fbff5b28ebf4a88c5e2
Robert Gardner <rdgardner.dev@gmail.com>
2023-08-16 15:34:59 +0000

    Convert to Vite

--- server/lib/authorization-middleware.ts
+++ server/lib/authorization-middleware.ts
@@ -1,13 +1,20 @@
+import { NextFunction, Request, Response } from 'express';
 import jwt from 'jsonwebtoken';
 import { ClientError } from './client-error.js';
 
-export function authorizationMiddleware(req, res, next) {
+const secret = process.env.TOKEN_SECRET ?? '';
+if (!secret) throw new Error('TOKEN_SECRET not found in env');
+
+export function authMiddleware(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
   // The token will be in the Authorization header with the format `Bearer ${token}`
   const token = req.get('authorization')?.split('Bearer ')[1];
   if (!token) {
     throw new ClientError(401, 'authentication required');
   }
-  const payload = jwt.verify(token, process.env.TOKEN_SECRET);
-  req.user = payload;
+  req.user = jwt.verify(token, secret) as Request['user'];
   next();
 }
--- server/lib/client-error.js
+++
@@ -1,8 +0,0 @@
-export default class ClientError extends Error {
-  status;
-
-  constructor(status, message) {
-    super(message);
-    this.status = status;
-  }
-}
