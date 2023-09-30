8dab16e7bdd9dbb83df5554697d48322b1e7f635
Robert Gardner <rdgardner.dev@gmail.com>
2023-09-30 19:33:40 +0000

    Respond with helpful message if no client

--- server/server.js
+++ server/server.js
@@ -2,7 +2,11 @@
 import 'dotenv/config';
 import express from 'express';
 import pg from 'pg';
-import { ClientError, errorMiddleware } from './lib/index.js';
+import {
+  ClientError,
+  errorMiddleware,
+  defaultMiddleware,
+} from './lib/index.js';
 
 const connectionString =
   process.env.DATABASE_URL ||
@@ -30,18 +34,7 @@ app.get('/api/hello', (req, res) => {
   res.json({ message: 'Hello, World!' });
 });
 
-/**
- * Serves React's index.html if no api route matches.
- *
- * Implementation note:
- * When the final project is deployed, this Express server becomes responsible
- * for serving the React files. (In development, the Vite server does this.)
- * When navigating in the client, if the user refreshes the page, the browser will send
- * the URL to this Express server instead of to React Router.
- * Catching everything that doesn't match a route and serving index.html allows
- * React Router to manage the routing.
- */
-app.get('*', (req, res) => res.sendFile(`${reactStaticDir}/index.html`));
+app.use(defaultMiddleware(reactStaticDir));
 
 app.use(errorMiddleware);
 
