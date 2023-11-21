319775dde4e7a30b7b75e0066f015ffd08558208
Robert Gardner <rdgardner.dev@gmail.com>
2023-11-21 00:47:52 +0000

    Fix lint error

--- server/lib/error-middleware.ts
+++ server/lib/error-middleware.ts
@@ -1,13 +1,13 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { NextFunction, Request, Response } from 'express';
 import jwt from 'jsonwebtoken';
 import { ClientError } from './client-error.js';
 
 export function errorMiddleware(
   err: unknown,
-  req: Request,
+  _req: Request,
   res: Response,
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars -- next must be declared for error middleware
-  next: NextFunction
+  _next: NextFunction
 ) {
   if (err instanceof ClientError) {
     res.status(err.status).json({ error: err.message });
